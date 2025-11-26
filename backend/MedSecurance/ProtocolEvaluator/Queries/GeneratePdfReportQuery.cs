52b72027ff656595ebbcf5454b507fc5fa10f038
George Stylianou <georgestylianou13@hotmail.com>
2025-11-26 17:42:46 +0200

    Introduce new endpoint to generate risk assesment report as PDF

---
+++ backend/MedSecurance/ProtocolEvaluator/Queries/GeneratePdfReportQuery.cs
@@ -0,0 +1,11 @@
+﻿using MedSecurance.ProtocolEvaluator.Models;
+using MedSecurance.ProtocolEvaluator.Models.Wifi;
+
+namespace MedSecurance.ProtocolEvaluator.Queries;
+
+public record GeneratePdfReportQuery(
+    ICollection<EvaluationSuggestion> Mitigations,
+    ICollection<EvaluationSuggestion> SafeConfigs,
+    ProtocolReplacementEvaluationResult Replacements,
+    ICollection<TvraCve>? Vulnerabilities
+);
